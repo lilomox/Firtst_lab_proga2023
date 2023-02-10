c6101845c6eda2bf82e1e2dd9d843dae78f805d7
sadesher <sadesher@student.21-school.ru>
2023-02-10 11:15:32 +0700

    lab #1 - is done

--- Ishodniki/force.c
+++ Ishodniki/force.c
@@ -15,7 +15,7 @@ void bubble_sort(int* array, int size) {
 	}
 }
 int main() {
-	printf("\n");
+	int array[100] = {10, 15, 5, 4, 21, 7};
 	bubble_sort(array, 6);
 	int i;
 	for (i = 0; i < 6 ; ++i) {
